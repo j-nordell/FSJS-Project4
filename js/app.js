0a7a3d6a722847fe56442e6c336bf41044017d0a
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-04-14 01:29:56 +0200

    input of name added

--- js/app.js
+++ js/app.js
@@ -12,6 +12,10 @@
 const startHTML = `<div class="screen screen-start" id="start">
                         <header>
                             <h1>Tic Tac Toe</h1>
+                            <form class="username" onSubmit="displayView(game.gameView)" name="form">
+                                <label for="username">Your name</label>
+                                <input class="input-styling" type="text" name="username" id="username" placeholder=""/>
+                            </form>
                             <a href="#" class="button" id="start-button">Start game</a>
                         </header>
                     </div>`;
@@ -20,12 +24,13 @@ const gameHTML = `<div class="board" id="board">
                     <header><h1>Tic Tac Toe</h1>
                         <ul>
                             <li class="players" id="player1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-200.000000, -60.000000)" fill="#000000"><g transform="translate(200.000000, 60.000000)"><path d="M21 36.6L21 36.6C29.6 36.6 36.6 29.6 36.6 21 36.6 12.4 29.6 5.4 21 5.4 12.4 5.4 5.4 12.4 5.4 21 5.4 29.6 12.4 36.6 21 36.6L21 36.6ZM21 42L21 42C9.4 42 0 32.6 0 21 0 9.4 9.4 0 21 0 32.6 0 42 9.4 42 21 42 32.6 32.6 42 21 42L21 42Z"/></g></g></g></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-200.000000, -60.000000)" fill="#000000"><g transform="translate(200.000000, 60.000000)"><path d="M21 36.6L21 36.6C29.6 36.6 36.6 29.6 36.6 21 36.6 12.4 29.6 5.4 21 5.4 12.4 5.4 5.4 12.4 5.4 21 5.4 29.6 12.4 36.6 21 36.6L21 36.6ZM21 42L21 42C9.4 42 0 32.6 0 21 0 9.4 9.4 0 21 0 32.6 0 42 9.4 42 21 42 32.6 32.6 42 21 42L21 42Z"/></g></g></g></svg>   
                             </li>
                             <li class="players" id="player2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="42" height="43" viewBox="0 0 42 43" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-718.000000, -60.000000)" fill="#000000"><g transform="translate(739.500000, 81.500000) rotate(-45.000000) translate(-739.500000, -81.500000) translate(712.000000, 54.000000)"><path d="M30 30.1L30 52.5C30 53.6 29.1 54.5 28 54.5L25.5 54.5C24.4 54.5 23.5 53.6 23.5 52.5L23.5 30.1 2 30.1C0.9 30.1 0 29.2 0 28.1L0 25.6C0 24.5 0.9 23.6 2 23.6L23.5 23.6 23.5 2.1C23.5 1 24.4 0.1 25.5 0.1L28 0.1C29.1 0.1 30 1 30 2.1L30 23.6 52.4 23.6C53.5 23.6 54.4 24.5 54.4 25.6L54.4 28.1C54.4 29.2 53.5 30.1 52.4 30.1L30 30.1Z"/></g></g></g></svg>
                             </li>
                         </ul>
+                        <h2></h2>
                     </header>
                     <ul class="boxes">
                         <li class="box" id="0"></li>
@@ -56,7 +61,6 @@ const finishHTML = ` <div class="screen screen-win" id="finish">
 class Player {
     constructor(name, type, icon) {
         this.name = name;
-        this.type = type;
         this.icon = icon;
     }
 }
@@ -67,8 +71,8 @@ class Game {
         this.gameWinner = 0;
         this.movesCount = 0;
         this.matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-        this.player1 = new Player("Player 1", "human", `url("img/o.svg")`);
-        this.player2 = new Player("Player 2", "human", `url("img/x.svg")`);
+        this.player1 = new Player("Player 1", `url("img/o.svg")`);
+        this.player2 = new Player("Computer", `url("img/x.svg")`);
         this.activePlayer = this.player1;
         this.startView = new View(startHTML, "start");
         this.gameView = new View(gameHTML, "board");
@@ -184,6 +188,8 @@ const newGameButton = document.getElementById("new-game");
 const playerOneBox = document.getElementById("player1");
 const playerTwoBox = document.getElementById("player2");
 const squares = document.getElementsByClassName("box");
+const playerNameInput = document.getElementById("username");
+
 
 // Get squares on game board and bind actions
 for(let square of squares) {
@@ -212,6 +218,7 @@ function displayView(viewToShow) {
             game.finishView.switchOff();
             game.gameView.switchOn();
             game.gameView.highlightPlayer(document.getElementById("player1"));
+            document.getElementsByTagName('h2')[0].textContent = `${game.player1.name} vs ${game.player2.name}`;
             break;
         case game.finishView:
             game.startView.switchOff();
@@ -222,6 +229,8 @@ function displayView(viewToShow) {
 
 // Make start game clickable and go to game view
 game.startView.bindUIActions(startButton, "click", () => {
+    game.player1.name = !playerNameInput.value ? "Human" : playerNameInput.value;
+    console.log(game.player1.name);
     displayView(game.gameView);
 });
 
